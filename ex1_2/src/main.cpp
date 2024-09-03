418a73bd615e4532ead3a61576f35687aaaa6ef3
Samuka007 <samuka007@qq.com>
2024-09-03 17:30:26 +0800

    feat: add timer, version 4 time test failed

--- ex1_2/src/main.cpp
+++ ex1_2/src/main.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
+#include <chrono>
 #include <cstring>
 #include <string>
 #include <map>
@@ -85,6 +87,48 @@ namespace test {
             assert(std::strcmp(p_simple.id, p_database.id) == 0);
         }
     }
+
+    // 需要测量两个时间。
+    // 1. 连续插入$N$条记录所需要的时间$T$。
+    // 2. 连续进行$M$次查找所需要的时间$T$。
+    // 请注意，$N$和$M$的取值范围是$10^3$到$10^6$。
+    auto timer(auto func) {
+        auto start = std::chrono::high_resolution_clock::now();
+        func();
+        auto end = std::chrono::high_resolution_clock::now();
+        return end - start;
+    }
+    
+    // class TestTime
+    // 在构造时生成$N$个随机id，形成person，保存在一个std::vector中，
+    // 这样方便插入时直接调用person_insert()。
+    // 依次从v1到v4的数据库中插入这$N$个id，测量插入时间。
+    // 依次从v1到v4的数据库中查找这$N$个id，测量查找时间。
+    class TestTime {
+    public:
+        TestTime(int N) {
+            if (N < 1000000) {
+                throw "N should be greater than 1000000";
+            }
+            for (int i = 0; i < N; i++) {
+                persons.push_back(new_person(random_id()));
+            }
+        }
+
+        void insert(int version) { // should only be called once for each version
+            set_version(version);
+            person_insert(persons.data(), persons.size());
+        }
+
+        void search() {
+            for (size_t i = 0; i < std::pow(10, 6); ++i) {
+                person_search(persons[i].id);
+            }
+        }
+
+    private:
+        std::vector<person> persons;
+    };
 }
 
 
@@ -93,6 +137,19 @@ int main(int argc, char** argv) {
         throw "random_id() should return a string with length 18";
     }
     for (int i = 1; i <= 4; i++) {
-        test::test_accuracy(i);
+        // test::test_accuracy(i);
+        test::TestTime test_time(1000000);
+        auto t = test::timer([&] {
+            test_time.insert(i);
+        });
+        auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t);
+        std::cout << "Insert time for version " << i << ": " 
+            << duration_ms << std::endl;
+        t = test::timer([&] {
+            test_time.search();
+        });
+        duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t);
+        std::cout << "Search time for version " << i << ": " 
+            << duration_ms << std::endl;
     }
 }
